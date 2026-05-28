990d916fc1fbc1782150f07cb73c4f02f114ddd8
AbolfazlAdhami <aadhami880@gmail.com>
2026-05-28 12:51:08 +0330

    sort app ts

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,11 @@
-console.log('hi there');
+import { LinkedList } from "./LinkedList";
 
-const logSomething = () => {
-  console.log('something');
-};
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print();
+console.log(linkedList);
