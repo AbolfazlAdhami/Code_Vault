cd61c3ddb8671ce15399662c1fdb5a5c0f2d61a7
AbolfazlAdhami <aadhami880@gmail.com>
2026-06-24 03:57:09 +0330

    new: react functional ts

---
+++ react-functional-ts/src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import EventComponent from "./events/EventComponent";
+
+const element = document.getElementById("root");
+
+const root = ReactDOM.createRoot(element!);
+
+const App = () => {
+  return (
+    <div>
+      <EventComponent />
+    </div>
+  );
+};
+
+root.render(<App />);
