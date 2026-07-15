440c75b63c2b201ce2e3b3afdc937b9f25e9b564
AbolfazlAdhami <aadhami880@gmail.com>
2026-07-15 12:12:27 +0330

    feat: server actions and sql database queries

--- nextjs-finance-app/app/page.tsx
+++ nextjs-finance-app/app/page.tsx
@@ -1,3 +1,9 @@
+import { createClient } from "@/libs/supabase/server";
+
 export default async function Home() {
+  const client = createClient();
+
+  console.log(await client.from("transactions").select());
+
   return <div>Hello</div>;
 }
