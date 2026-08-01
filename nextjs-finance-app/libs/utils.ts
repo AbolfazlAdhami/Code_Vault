80cf83cf896400479bfd4555e699b1c46a851530
AbolfazlAdhami <aadhami880@gmail.com>
2026-08-02 01:36:03 +0330

    fix: fix types in utils type transactionsum

--- nextjs-finance-app/libs/utils.ts
+++ nextjs-finance-app/libs/utils.ts
@@ -1,22 +1,46 @@
-interface TransactionType {
+enum TransactionType {
+  Income = "Income",
+  Expense = "Expense",
+  Saving = "Saving",
+  Investment = "Investment",
+}
+
+interface TransactionPropsType {
   id: string;
   type: TransactionType;
   category?: string;
   description?: string;
   amount: number;
-  create_at: Date;
+  created_at: string;
+  onRemoved?: () => void;
 }
 
-export const groupAndSumTransactionsByDate = (transactions) => {
-  const grouped = {};
+interface GroupedTransaction {
+  transactions: TransactionPropsType[];
+  amount: number;
+}
+
+type GroupedTransactions = Record<string, GroupedTransaction>;
+
+export const groupAndSumTransactionsByDate = (transactions: TransactionPropsType[]): GroupedTransactions => {
+  const grouped: GroupedTransactions = {};
+
   for (const transaction of transactions) {
     const date = transaction.created_at.split("T")[0];
+
     if (!grouped[date]) {
-      grouped[date] = { transactions: [], amount: 0 };
+      grouped[date] = {
+        transactions: [],
+        amount: 0,
+      };
     }
+
     grouped[date].transactions.push(transaction);
-    const amount = transaction.type === "Expense" ? -transaction.amount : transaction.amount;
+
+    const amount = transaction.type === TransactionType.Expense ? -transaction.amount : transaction.amount;
+
     grouped[date].amount += amount;
   }
+
   return grouped;
 };
